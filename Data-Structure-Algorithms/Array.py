8970af00c74bae2f09975e5be6acd91e8ecefa60
ravii17 <ravikumar75890@gmail.com>
2026-08-11 17:21:59 +0530

    sorting array

---
+++ Data-Structure-Algorithms/Array.py
@@ -0,0 +1,10 @@
+nums = [30,40,6,5,4,2,9,0]
+#print(a)
+
+#print ( 6 in a )  #this is used to search the int in the array
+#print(a.count(7) > 0)   
+
+nums.sort()
+print(nums)
+
+print(sorted(nums, reverse=True)) 
---
+++ private_entity.py
@@ -0,0 +1,12 @@
+class account:
+    def __init__ (self, account_no, account_password):
+        self.account_no = account_no
+        self.account_password = account_password
+
+    def reset_pass(Set):
+        print(self.account_password)
+
+accounr1= account("1234" , "!@#$")
+account2= account("8926", "abcd")
+
+print(account2.account_no)
